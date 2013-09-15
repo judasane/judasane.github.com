ef1b69f8e9c0d960ebe0df9f43051c1f7759a7fa
Juan <Juan@Laptop-W8>
2013-09-15 14:42:23 -0500

    Desgned by me

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="styles.css"/>
+        <title>Soy Juan Sánchez, Bienvenido</title>
+    </head>
+    <body>
+        <div id="main_container">
+            <header>
+                <span class="code_tag" >class</span> <h1>JuanSánchez</h1>{
+            </header>
+            <div id="img_me">
+                <div class="comments" id="description">
+                    /*Hola, soy Juan, un chico inquieto por el desarrollo web, amante de la innovación y dispuesto a asumir los 
+                    retos a los que me enfrentela vida.*/ 
+                </div>
+                <img src="img/sanchezneme.png" />
+            </div>
+            <nav>
+                <div>
+                    <span class="code_tag">public</span>
+                    <a href="about_me.php">$acercaDeMi</a>
+                </div>
+                <div>
+                    <span class="code_tag">public</span>
+                    <a>$proyectos</a>
+                </div>
+                <div>
+                    <span class="code_tag">public</span>
+                    <a>$social</a>
+                </div>
+                <div>
+                    <span class="code_tag">public</span>
+                    <a>$blog</a>
+                </div>
+                <div>
+                    <span class="code_tag">public</span>
+                    <a>$servicios</a>
+                </div>
+                <div>
+                    <span class="code_tag">public</span>
+                    <a>$vidaPersonal</a>
+                </div>
+                <div>
+                    <span class="code_tag">public function</span>
+                    <a>contacto()</a>
+                </div>
+            </nav>
+        </div>
+    </body>
+</html>
